c74596ea8874f0d51c7013db41f0844730b9694d
Rumen Donchev <r.donchev@protonmail.com>
2018-01-28 16:18:49 +0100

    write with a service

---
+++ ExpenseTracker/app/src/main/java/project/simple/expensetracker/services/EntryService.java
@@ -0,0 +1,17 @@
+package project.simple.expensetracker.services;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by e6420 on 28.01.18.
+ */
+
+public class EntryService extends IntentService
+{
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+    }
+}
