e3b87564afdb8efe2de97e69baed4c9f4be0753d
Rumen Donchev <r.donchev@protonmail.com>
2018-01-31 21:42:51 +0100

    entry

--- ExpenseTracker/app/src/main/java/project/simple/expensetracker/EntryActivity.java
+++ ExpenseTracker/app/src/main/java/project/simple/expensetracker/EntryActivity.java
@@ -2,6 +2,9 @@ package project.simple.expensetracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class EntryActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class EntryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
+        Button button= (Button) findViewById(R.id.addEntry);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO call your code here. Disregard the service. No time!
+                Toast toast = Toast.makeText(getApplicationContext(), "TEST", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
     }
 }
